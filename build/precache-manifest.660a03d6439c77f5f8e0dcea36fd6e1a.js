d361a698887e9fe02d4f8989d26d7fb0870d4e77
deepak <techwinner20@gmail.com>
2020-05-18 18:52:38 -0700

    storybook

---
+++ build/precache-manifest.660a03d6439c77f5f8e0dcea36fd6e1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9114ad83dbdbc75999da7b612a665d23",
+    "url": "/Storybook-React/index.html"
+  },
+  {
+    "revision": "e45b90dd83a84b1b5d87",
+    "url": "/Storybook-React/static/css/2.59222f74.chunk.css"
+  },
+  {
+    "revision": "27563042dcf1786b649b",
+    "url": "/Storybook-React/static/css/main.da6663f7.chunk.css"
+  },
+  {
+    "revision": "e45b90dd83a84b1b5d87",
+    "url": "/Storybook-React/static/js/2.836abf87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Storybook-React/static/js/2.836abf87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27563042dcf1786b649b",
+    "url": "/Storybook-React/static/js/main.6481c706.chunk.js"
+  },
+  {
+    "revision": "db2bb2170b52ddba9a73",
+    "url": "/Storybook-React/static/js/runtime-main.970863d4.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Kutumb from 'kutumb-style';
+function App() {
+
+  return (
+    <div className="App">
+     storybook
+     </div>
+  );
+}
+
+export default App;
